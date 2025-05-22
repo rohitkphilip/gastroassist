f3d335d9075a2991674221944668d86ee9237f74
rohitkphilip <rohitkurian.philip@sjsu.edu>
2025-05-21 22:26:24 -0500

    Update next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: 'https://gastroapi.rohitkphilip.com/api/:path*',
       },
     ];
   },
